716972cf24a5735b9edc53c6d5cda62ac19fbc4e
robert.lee <robert0714@gmaill.com>
2019-02-04 22:18:09 +0800

    adjust

--- Chapter15/p520_writing_e2e_tests_with_Gherkin_Cucumber_and_Selenium/hobnob-client/spec/cucumber/steps/assertions/index.js
+++ Chapter15/p520_writing_e2e_tests_with_Gherkin_Cucumber_and_Selenium/hobnob-client/spec/cucumber/steps/assertions/index.js
@@ -1,18 +1,8 @@
 import assert from 'assert';
-import chai, { expect } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import { Given, When, Then } from 'cucumber';
-import { By, until } from 'selenium-webdriver';
-
-chai.use(chaiAsPromised);
-
-Then(/^the (?:"|')([\.#\w-]+)(?:"|') element should( not)? have a (?:"|')([\w_-]+)(?:"|') attribute$/, async function (selector, negation, attributeName) {
+import { By } from 'selenium-webdriver';
+When(/^the (?:"|')([\.#\w-]+)(?:"|') element should have a (?:"|')([\w_-]+)(?:"|') attribute$/, async function (selector,attributeName) {
   const element = await this.driver.findElement(By.css(selector));
   const attributeValue = await element.getAttribute(attributeName);
-  const expectedValue = negation ? null : attributeValue;
-  assert.equal(attributeValue, expectedValue);
-});
-
-Then(/^the (?:"|')([\.#\w-]+)(?:"|') element should appear within (\d+) miliseconds$/, function (selector, timeout) {
-  return expect(this.driver.wait(until.elementLocated(By.css(selector)), timeout)).to.be.fulfilled;
+  assert.equal(attributeValue, 'true');
 });
